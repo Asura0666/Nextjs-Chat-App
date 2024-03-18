caf65c413a502120b44e2017a1980873196fe265
Asura0666 <dhirajlande18@gmail.com>
2024-03-18 17:42:06 +0530

    add stream chat and connect stream and clerk with project !!

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,7 +1,60 @@
-import { UserButton } from "@clerk/nextjs";
+"use client";
+
+import { UserButton, useUser } from "@clerk/nextjs";
+import {
+  Channel,
+  ChannelHeader,
+  ChannelList,
+  Chat,
+  LoadingIndicator,
+  MessageInput,
+  MessageList,
+  Thread,
+  Window,
+} from "stream-chat-react";
+import useInitializeChatClient from "./useInitializeChatClient";
 
 export default function ChatPage() {
-  return <div>This is the chat
-    <UserButton afterSignOutUrl="/"/>
-  </div>
+  const chatClient = useInitializeChatClient();
+  const { user } = useUser();
+
+  if (!chatClient || !user) {
+    return (
+      <div className="flex h-screen items-center justify-center">
+        <LoadingIndicator size={40} />
+      </div>
+    );
+  }
+
+  return (
+    <div className="h-screen">
+      <Chat client={chatClient}>
+        <div className="flex h-full flex-row">
+          <div className="w-full max-w-[360px]">
+            <ChannelList
+              filters={{
+                type: "messaging",
+                members: { $in: [user.id] },
+              }}
+              sort={{ last_message_at: -1 }}
+              options={{ state: true, presence: true, limit: 10 }}
+            />
+          </div>
+          <div className="w-full h-full">
+            <Channel>
+              <Window>
+                <ChannelHeader />
+                <MessageList />
+                <MessageInput />
+              </Window>
+              <Thread />
+            </Channel>
+          </div>
+        </div>
+      </Chat>
+    </div>
+  );
 }
+
+// This is the chat
+// <UserButton afterSignOutUrl="/" />
